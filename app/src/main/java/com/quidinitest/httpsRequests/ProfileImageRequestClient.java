2b8f40859a59b05e3ee5f0e17a7ae7ab59f09518
AYO <ayooladedeji@live.com>
2016-12-08 19:55:54 +0000

    [WIP]

--- app/src/main/java/com/quidinitest/httpsRequests/ProfileImageRequestClient.java
+++ app/src/main/java/com/quidinitest/httpsRequests/ProfileImageRequestClient.java
@@ -1,12 +1,20 @@
 package com.quidinitest.httpsRequests;
 
+import android.Manifest;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.quidinitest.R;
+import com.quidinitest.permissions.Permissions;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.security.MessageDigest;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class ProfileImageRequestClient {
@@ -14,20 +22,29 @@ public class ProfileImageRequestClient {
     private static final String mTAG = "ProfileImageRequest";
     private static final String URL = "http://www.gravatar.com/avatar/";
 
+    //private static ExecutorService executorService = Executors.newFixedThreadPool(2);
+
     public static void loadImageView(final ImageView imageView, final String email, final Context context) {
 
-        new Thread(new Runnable() {
+        Picasso.with(context).load(URL+emailToHash(email)).into(new Target() {
             @Override
-            public void run() {
-                try{
-                    Picasso.with(context)
-                            .load(URL+emailToHash(email))
-                            .into(imageView);
-                } catch(Exception e){
-                    Log.d(mTAG, e.getMessage());
-                }
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Log.i(mTAG, "The image was obtained correctly");
+                imageView.setImageBitmap(bitmap);
             }
-        }).start();
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                Log.e(mTAG, "The image was not obtained");
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                Log.i(mTAG, "Getting ready to get the image");
+                //Here you should place a loading gif in the ImageView to
+                //while image is being obtained.
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/quidinitest/utilities/TimeUtils.java
@@ -0,0 +1,8 @@
+package com.quidinitest.utilities;
+
+/**
+ * Created by ayoola on 08/12/2016.
+ */
+
+public class TimeUtils {
+}
